b3e2c9ce50d8d646e52db58c4cf0fcc6341038f2
root <root@r1riepas.schedulebull.com>
2022-10-10 10:28:27 +0000

    Link fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link rel="icon" type="image/vnd.microsoft.icon" href="{{ asset('images/favicon.ico') }}">
     <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
     <meta name="csrf-token" content="{!! csrf_token() !!}">
-    <meta name="verify-paysera" content="8edf175c7d27ddd50c1f859814a5812f">
+    <meta name="verify-paysera" content="8efacf3cf88620d4c363c6eb973712bb">
     <meta name="description" content="R1Riepas">
     <meta name="keywords" content="riepas, diski, kondicionieris, montāža, balansēšana, riepu diski">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -122,7 +122,7 @@
                                   <div class="modal-dialog">
                                     <div class="modal-content">
                                       <div class="modal-body">
-                                        <a class="popup-close cls-btn" aria-label="close" data-dismiss="modal" href="#" data-bs-toggle="modal" data-bs-target="#popup-1" data-bs-dismiss="modal" aria-label="Close">x</a>
+                                        <a class="popup-close cls-btn" aria-label="close" data-dismiss="modal" href="#" data-target="#popup-1" data-dismiss="modal">x</a>
                                         <div id="map"></div>
                                       </div>
                                     </div>
@@ -349,7 +349,7 @@
                                 </div>
 
                                 <div class="top-map">
-                                    <button id="map-modal-toggle" type="button" data-bs-toggle="modal" data-bs-target="#popup-1">
+                                    <button id="map-modal-toggle" type="button" data-toggle="modal" data-target="#popup-1">
                                         karte </button>
                                 </div>
                             </div>
@@ -742,7 +742,7 @@
 <script type="text/javascript" src="{{ asset('js/homeslider.js?rev=' . time()) }}"></script>
 <script type="text/javascript" src="{{ asset('js/custom.js?rev=' . time()) }}"></script>
 <script type="text/javascript" src="{{ asset('js/cart.js?rev=' . time()) }}"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-76Y13VND83"></script>
 <script>
