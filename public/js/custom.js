7a0e4c988d445875318906a5b439513a3d5711eb
Rihards Ādminis <juriskalns124@gmail.com>
2022-10-11 14:07:30 +0300

    Validation translation

--- public/js/custom.js
+++ public/js/custom.js
@@ -3026,3 +3026,43 @@ $('.cart-card .checkout-buttons .form-check input[name=payment]').each(function(
     }
   });
 });
+
+// Validācija
+document.addEventListener("DOMContentLoaded", function() {
+  // Visi input fieldi
+  let elements = $('input');
+  for (let i = 0; i < elements.length; i++) {
+    // Noņem popup kad hovero pāri required fieldam
+    elements[i].title = '';
+    // Uz invalīda fielda atgriež validācijas tekstu
+    elements[i].oninvalid = function(e) {
+      e.target.setCustomValidity("");
+      if (!e.target.validity.valid) {
+        e.target.setCustomValidity("Šis lauks nedrīkst būt tukšs!");
+      }
+    };
+    elements[i].oninput = function(e) {
+      e.target.setCustomValidity("");
+    };
+  }
+})
+
+// jQuery.extend(jQuery.validator.messages, {
+//   required:   "This field is required.",
+//   remote:     "Please fix this field.",
+//   email:      "Please enter a valid email address.",
+//   url:        "Please enter a valid URL.",
+//   date:       "Please enter a valid date.",
+//   dateISO:    "Please enter a valid date (ISO).",
+//   number:     "Please enter a valid number.",
+//   digits:     "Please enter only digits.",
+//   creditcard: "Please enter a valid credit card number.",
+//   equalTo:    "Please enter the same value again.",
+//   accept:     "Please enter a value with a valid extension.",
+//   maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+//   minlength: jQuery.validator.format("Please enter at least {0} characters."),
+//   rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+//   range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+//   max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+//   min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+// });
