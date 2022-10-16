ef3ecddeb1c5ffdfdf9174fcb4e48f754a5db965
Rihards Ādminis <juriskalns124@gmail.com>
2022-10-16 13:28:39 +0300

    Pakalpojumi scroll fix

--- resources/views/pages/components/pakalpojumi.blade.php
+++ resources/views/pages/components/pakalpojumi.blade.php
@@ -1,4 +1,5 @@
 <p style="text-align: center;"><strong><span style="font-size: 18pt;">RIEPU NOMAIŅA</span></strong></p>
+<div style="overflow-x: auto;">
 <table dir="ltr" style="width: 100.638%; height: 223.906px;" border="1" cellspacing="0" cellpadding="0"><colgroup><col style="width: 26.8116%;" width="63"><col style="width: 15.2174%;" width="63"><col style="width: 15.2174%;" width="63"><col style="width: 21.4674%;" width="63"><col style="width: 21.3768%;" width="63"></colgroup>
 <tbody>
 <tr style="height: 22.3906px;">
@@ -69,8 +70,10 @@
 </tr>
 </tbody>
 </table>
+</div>
 <p style="text-align: center;"> </p>
 <p style="text-align: center;"><strong><span style="font-size: 18pt;">MOTOCIKLIEM, MOTOROLLERIEM</span></strong></p>
+<div style="overflow-x: auto;">
 <table dir="ltr" style="width: 101.275%;" border="1" cellspacing="0" cellpadding="0"><colgroup><col style="width: 26.0126%;" width="63"><col style="width: 17.1017%;" width="63"><col style="width: 15.3915%;" width="63"><col style="width: 24.7598%;" width="63"><col style="width: 16.7344%;" width="63"></colgroup>
 <tbody>
 <tr>
@@ -111,6 +114,7 @@
 </tr>
 </tbody>
 </table>
+</div>
 <p style="text-align: center;"> </p>
 <p style="text-align: center;"><strong><span style="font-size: 18pt;">KVADRACIKLIEM</span></strong></p>
 <table dir="ltr" style="width: 101.093%; height: 156.734px;" border="1" cellspacing="0" cellpadding="0"><colgroup><col style="width: 49.5816%;" width="63"><col style="width: 50.4184%;" width="63"></colgroup>
@@ -147,6 +151,7 @@
 </table>
 <p style="text-align: center;"> </p>
 <p style="text-align: center;"><strong><span style="font-size: 18pt;">RITEŅU MONTĀŽA</span></strong></p>
+<div style="overflow-x: auto;">
 <table dir="ltr" style="width: 100.93%; height: 179.094px;" border="1" cellspacing="0" cellpadding="0"><colgroup><col style="width: 21.2264%;" width="222"><col style="width: 19.5069%;" width="57"><col style="width: 14.6315%;" width="61"><col style="width: 16.4378%;" width="57"><col style="width: 17.07%;" width="57"><col style="width: 11.0188%;" width="57"></colgroup>
 <tbody>
 <tr style="height: 22.3906px;">
@@ -204,7 +209,9 @@
 </tr>
 </tbody>
 </table>
+</div>
 <p> </p>
+<div style="overflow-x: auto;">
 <table dir="ltr" style="width: 100.543%; height: 156.734px;" border="1" cellspacing="0" cellpadding="0"><colgroup><col style="width: 21.668%;" width="222"><col style="width: 21.7609%;" width="57"><col style="width: 17.3171%;" width="61"><col style="width: 18.4051%;" width="57"><col style="width: 20.7624%;" width="57"></colgroup>
 <tbody>
 <tr style="height: 22.3906px;">
@@ -254,6 +261,7 @@
 </tr>
 </tbody>
 </table>
+</div>
 <p> </p>
 <p style="text-align: center;"><strong><span style="font-size: 18pt;">RITEŅU BALANSĒŠANA</span></strong></p>
 <p style="text-align: center;"><strong><span style="font-size: 18pt;"><span data-sheets-formula-bar-text-style="font-size:16px;color:#000000;font-weight:bold;text-decoration:none;font-family:'Times New Roman';font-style:normal;text-decoration-skip-ink:none;">RITEŅU BALANSĒŠANA VIEGLAJIEM AUTO </span><span data-sheets-formula-bar-text-style="font-size:16px;color:#000000;font-weight:bold;text-decoration:underline;font-family:''Times New Roman'';font-style:normal;text-decoration-skip-ink:none;">AR NOMAIŅU UZ AUTO</span></span></strong></p>
