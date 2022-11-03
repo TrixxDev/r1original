34e0ec7ad1fad3618134aab6d434d2981e30e14e
root <root@r1riepas.schedulebull.com>
2022-11-03 11:17:57 +0200

    Tires fix

--- app/Http/Controllers/QuadTireController.php
+++ app/Http/Controllers/QuadTireController.php
@@ -114,10 +114,13 @@ class QuadTireController extends Controller
             ->where('quadr_tires.tire_id', $tire)
             ->first();
 
+	$currBrand = Quadrbrand::where('brand_id', $tread->brand_id)->first();
+	//dd(\App\Helper\Image::showAd('quadr', $currTire->tread_id));
+
         $currTire->includeStock = true;
 
         return view('tires.quadr.quadrtread',
-            compact('tires', 'currTire')
+            compact('tires', 'currTire', 'currBrand')
         );
     }
 
