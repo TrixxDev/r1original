eaed5ec6111e7223d6f5a4f2349742275d5b11a3
Edgars <indrikis38@gmail.com>
2022-12-06 15:51:16 +0200

    Moto tires fix

--- app/Models/Moto.php
+++ app/Models/Moto.php
@@ -224,14 +224,14 @@ class Moto extends Model
     public function getLinkAttribute()
     {
         $tire = Mototread::selectRaw('moto_treads.*, moto_treads.title as tread_title')
-            ->selectRaw('moto_brands.*, moto_brands.slug as brand_title')
+            ->selectRaw('moto_brands.*, moto_brands.title as brand_title')
             ->leftJoin('moto_brands', 'moto_treads.brand_id', '=', 'moto_brands.brand_id')
             ->where('moto_treads.tread_id', $this->make_id)
             ->first();
         if (!isset($tire->brand_title) || !isset($tire->tread_title)) {
             return false;
         } else {
-            return route('motociklu-riepa', [$tire->brand_title, str_replace('/', '_', $tire->tread_title), $this->tire_id]);
+            return route('motociklu-riepa', [strtolower($tire->brand_title), str_replace('/', '_', $tire->tread_title), $this->tire_id]);
         }
     }
 
