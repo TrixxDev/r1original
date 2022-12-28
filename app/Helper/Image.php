311d30f36741ffc46c89e06f3429f43814d899a7
Rihards Ādminis <juriskalns124@gmail.com>
2022-12-28 14:55:19 +0200

    Image helper fix

--- app/Helper/Image.php
+++ app/Helper/Image.php
@@ -40,14 +40,6 @@
         case 'big-rim':
           $dir = $path . '/industrial/rims/rim-' . $image . '.jpg';
           break;
-
-        // AUTO RIMS
-        case 'rim':
-          $dir = $path . '/auto/rim/' . $image . '-o.jpg';
-          break;
-        case 'big-rim':
-          $dir = $path . '/auto/rim/rim-' . $image . '.jpg';
-          break;
       }
 
       return $dir;
