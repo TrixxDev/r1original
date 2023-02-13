b87f838c8b0755992bcbfeba678d77cc6fbf97b7
Edgars <indrikis38@gmail.com>
2023-02-13 11:21:02 +0200

    Admin Moto image fix

--- resources/views/admin/moto_tires/index.blade.php
+++ resources/views/admin/moto_tires/index.blade.php
@@ -57,7 +57,6 @@
 {{--                                  {{ dd($tread) }}--}}
                                   {!! App\Helper\Image::showGrid('moto', $tread->tread_id, 'width: 300px; margin-bottom: 20px;') !!}
 
-                                  @if (isset($tread->image))
                                   <form action="{{ route('admin.auto.tires.image', $tread->tread_id) }}" method="post" enctype="multipart/form-data">
                                       @csrf
                                       <div class="row">
@@ -72,7 +71,6 @@
                                           </div>
                                       </div>
                                   </form>
-                                  @endif
                                 @endif
                             </div>
                             @if (isset($tread))
