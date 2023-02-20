54c64cf53f75ed8014b034904ba06975b5feb224
Edgars <indrikis38@gmail.com>
2023-02-20 15:08:13 +0200

    AutoTireController image upload directory fix

--- app/Http/Controllers/Admin/AutoTireController.php
+++ app/Http/Controllers/Admin/AutoTireController.php
@@ -328,19 +328,19 @@ class AutoTireController extends Controller
             $fileNameMed   = $id . '-n';
             $fileNameLarge   = $id . '-o';
 //            dd($image);
-            Image::make($image->getRealPath())->save('public/storage/auto/tread/' . $fileName . '.jpg');
+            Image::make($image->getRealPath())->save('storage/auto/tread/' . $fileName . '.jpg');
             Image::make($image->getRealPath())
               ->resize(100, 100, function($constraint) {
                 $constraint->aspectRatio();
-              })->save('public/storage/auto/tread/' . $fileNameSmall . '.jpg');
+              })->save('storage/auto/tread/' . $fileNameSmall . '.jpg');
             Image::make($image->getRealPath())
               ->resize(200, 200, function($constraint) {
                 $constraint->aspectRatio();
-              })->save('public/storage/auto/tread/' . $fileNameMed . '.jpg');
+              })->save('storage/auto/tread/' . $fileNameMed . '.jpg');
             Image::make($image->getRealPath())
               ->resize(1500, 1500, function($constraint) {
                 $constraint->aspectRatio();
-              })->save('public/storage/auto/tread/' . $fileNameLarge . '.jpg');
+              })->save('storage/auto/tread/' . $fileNameLarge . '.jpg');
         }
         return redirect()->back();
     }
