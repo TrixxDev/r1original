78b53dbdad21f7f3025cc411515371201ff0bc63
Edgars <indrikis38@gmail.com>
2023-05-03 16:04:16 +0300

    Small Patch

--- app/Http/Controllers/Records/RecordController.php
+++ app/Http/Controllers/Records/RecordController.php
@@ -1382,7 +1382,7 @@
 
                 $cURLConnection = curl_init();
 
-                curl_setopt($cURLConnection, CURLOPT_URL, 'http://api.textmebot.com/send.php?recipient=120363130984594947@g.us&apikey=ATpfS4ctcJUW&text=Atcelts%20pieraksts%20-%20' . $time . '%20' . $vehicle . '%20' . $model);
+                curl_setopt($cURLConnection, CURLOPT_URL, 'http://api.textmebot.com/send.php?recipient=120363130984594947@g.us&apikey=d6nsRWNp1xpc&text=Atcelts%20pieraksts%20-%20' . $time . '%20' . $vehicle . '%20' . $model);
                 curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
 
                 curl_exec($cURLConnection);
@@ -1391,7 +1391,7 @@
               } else {
                 $cURLConnection = curl_init();
 
-                curl_setopt($cURLConnection, CURLOPT_URL, 'http://api.textmebot.com/send.php?recipient=120363150684433547@g.us&apikey=ATpfS4ctcJUW&text=Atcelts%20pieraksts%20-%20' . $time . '%20' . $vehicle . '%20' . $model);
+                curl_setopt($cURLConnection, CURLOPT_URL, 'http://api.textmebot.com/send.php?recipient=120363150684433547@g.us&apikey=d6nsRWNp1xpc&text=Atcelts%20pieraksts%20-%20' . $time . '%20' . $vehicle . '%20' . $model);
                 curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
 
                 curl_exec($cURLConnection);
