82888f3d763bd3c0c62c941e252f971e7e368763
Edgars <indrikis38@gmail.com>
2023-05-25 09:40:55 +0300

    Nomainīti sinhronizāciju failu ceļi (I3 Big/Truck)

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -1753,7 +1753,7 @@
 
       Bigstock::where('itype', 'i3')->where('type', 'agro')->update(['quantity' => 0]);
 
-      $content = file_get_contents(dirname(__DIR__, 3) . '/public/storage/xml/i3-agro.txt';);
+      $content = file_get_contents(dirname(__DIR__, 3) . '/public/storage/xml/i3-agro.txt');
       $content = json_decode($content);
 
       $returnText = '';
