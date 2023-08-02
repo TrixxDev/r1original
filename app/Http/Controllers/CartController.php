533520a1650e953d01de6ffe5d926968aa141915
Edgars <indrikis38@gmail.com>
2023-08-02 15:47:09 +0300

    Izlabots, lai pareizi uz epastu sūta summu

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -716,7 +716,8 @@ class CartController extends Controller
                 <td style="text-align: center;"></td>
                 <td style="text-align: center;"><b>Kopā:</b></td>';
                 if ($details->delivery_price > 0) {
-                  $out .= '<td style="text-align: center;">€ ' . ((int) $details->price + (int) substr($details->delivery_price, 0, 2)) . '</td>';
+                  $delivery_price = intval(rtrim(strval($details->delivery_price), '0'));
+                  $out .= '<td style="text-align: center;">€ ' . ((int) $details->price + $delivery_price) . '</td>';
                 } elseif ($details->fit_price > 0) {
                   $out .= '<td style="text-align: center;">€ ' . ((int) $details->price + (int) substr($details->fit_price, 0, 2)) . '</td>';
                 } else {
