c41a2453e80ed710156a65e28b8827693af418d4
Edgars <indrikis38@gmail.com>
2023-11-29 10:01:04 +0200

    Bug fix

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -170,7 +170,7 @@
                                                       if (date('Y-m-d') == $workingDay->date) {
                                                         if (\Carbon\Carbon::parse($currentTime)->subHour() >= \Carbon\Carbon::now()) {
                                                           $slotClass = 'taken-slot';
-                                                          if ($slot->comment !== null) {
+                                                          if ($slot->comment !== null && is_null($slot->takenby)) {
                                                             $slotClass = 'time-free';
                                                           }
                                                         }
