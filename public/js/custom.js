739f2de14e52532535e1a2461e53ed0ad3483499
Edgars <indrikis38@gmail.com>
2024-02-08 09:33:59 +0200

    Trying to fix loading block

--- public/js/custom.js
+++ public/js/custom.js
@@ -3187,7 +3187,7 @@ function fadeOut(element) {
 
 window.addEventListener('load', function() {
   // Check if the page is being loaded from the back-forward cache
-  if (performance.getEntriesByType("navigation")[0].type || performance.getEntriesByType("navigation")[0].type === 'back_forward') {
+  if (performance.getEntriesByType("navigation")[0].type === 'back_forward') {
     // Page is loaded from cache or a prerender
     isPageLoadedFromCache = true;
   }
