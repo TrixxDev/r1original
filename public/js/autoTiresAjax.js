160110deff0837705f26867271cc190a98c0e3a9
Edgars <indrikis38@gmail.com>
2024-03-20 15:13:15 +0200

    Auto tire update

--- public/js/autoTiresAjax.js
+++ public/js/autoTiresAjax.js
@@ -101,8 +101,7 @@ $(document).ready(function() {
     pageNr = 1;
     loadItems();
     if (window['top_enabled'].length <= 0) {
-      $('#search_filters #show-top-checkbox').attr('checked', true).prop('checked', true);
-      window['top_enabled'] = '&top=show';
+      $('#search_filters #show-top-checkbox').trigger('click');
     }
   });
 
@@ -133,8 +132,7 @@ $(document).ready(function() {
     if ($(this).val().length > 0) {
       loadItems();
       if (window['top_enabled'].length <= 0) {
-        $('#search_filters #show-top-checkbox').attr('checked', true).prop('checked', true);
-        window['top_enabled'] = '&top=show';
+        $('#search_filters #show-top-checkbox').trigger('click');
       }
     }
   });
