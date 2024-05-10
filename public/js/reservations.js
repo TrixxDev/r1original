4c18193b539096a2bf35d318a1ce691272fed623
Edgars <indrikis38@gmail.com>
2024-05-10 16:36:27 +0300

    Update: 2 checkboxes for AC and MOTO toggles in queues settings

--- public/js/reservations.js
+++ public/js/reservations.js
@@ -167,6 +167,10 @@ $(document).ready(function() {
     let newCloseTime = $('#queueModal select#f_closetime option:selected').val();
     let timeStep = $('#queueModal #f_timeinterval option:selected').val();
     let is_half = parseInt($('#queueModal input[name="queue"]:checked').val());
+    let ac_toggle = $('#queueModal input[name="ac_toggle"]').attr('checked');
+    let moto_toggle = $('#queueModal input[name="moto_toggle"]').attr('checked');
+
+    console.log(ac_toggle, moto_toggle);
 
     let sendData = {
       'times': {
