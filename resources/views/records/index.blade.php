61e77bd52f171a12dc30e01216635877fabfc07f
Edgars <indrikis38@gmail.com>
2024-07-16 09:31:24 +0300

    Schedule fix - If half queue without MOTO and AC then show free slots with taken slots

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -142,14 +142,14 @@
                                                             if (!is_null($halfMotoService)) {
                                                               $content = '<div class="time-status flex time-free" data-iorder="' . $i . '" data-moto="true"><div class="time-slot">' . $currentTime . '</div><button class="status free-slot-link available-slot">Moto montāža</button></div>';
                                                             } else {
-                                                              $content = '<div class="time-status flex time-taken-half" data-iorder="' . $i . '"><div class="time-slot">' . $currentTime . '</div><div class="slot taken-slot">Aizņemts</div></div>';
+                                                              $content = '<div class="time-status flex time-taken-half" data-iorder="' . $i . '" data-moto="true"><div class="time-slot">' . $currentTime . '</div><div class="slot taken-slot">Aizņemts</div></div>';
                                                             }
                                                           } else {
                                                             $content = '<div class="time-status flex time-closed" data-iorder="' . $i . '"></div>';
                                                           }
                                                         } else {
                                                           if ($i >= 0) {
-                                                            $content = '<div class="time-status flex ' . $slotClass . '" data-iorder="' . $i . '"><div class="time-slot">' . $currentTime . '</div><div class="slot unavailable taken-slot">Aizņemts</div></div>';
+                                                            $content = '<div class="time-status flex ' . $slotClass . '" data-iorder="' . $i . '" data-moto="true"><div class="time-slot">' . $currentTime . '</div><div class="slot unavailable taken-slot">Aizņemts</div></div>';
                                                           } else {
                                                             $content = '<div class="time-status flex time-closed" data-iorder="' . $i . '"></div>';
                                                           }
