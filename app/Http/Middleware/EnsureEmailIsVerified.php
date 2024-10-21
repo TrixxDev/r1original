eb86367979f3964265eff79544c232b87e88c38d
Edgars <indrikis38@gmail.com>
2024-10-21 15:17:12 +0300

    Disabled user verify system

--- app/Http/Middleware/EnsureEmailIsVerified.php
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -14,15 +14,15 @@ class EnsureEmailIsVerified
 //      return redirect()->route('verification.notice');
 //    }
 
-    if (Auth::check() && !Auth::user()->hasVerifiedEmail() && !$request->is('email/*', 'logout')) {
-      $register_date = Carbon::parse(Auth::user()->created_at);
-      $now_date = Carbon::now();
-      if ($now_date->diffInDays($register_date) < 1) {
-        return true;
-      } else {
-        return redirect()->route('verification.notice');
-      }
-    }
+//    if (Auth::check() && !Auth::user()->hasVerifiedEmail() && !$request->is('email/*', 'logout')) {
+//      $register_date = Carbon::parse(Auth::user()->created_at);
+//      $now_date = Carbon::now();
+//      if ($now_date->diffInDays($register_date) < 1) {
+////        return true;
+//      } else {
+//        return redirect()->route('verification.notice');
+//      }
+//    }
 
     return $next($request);
   }
