2d0de2a2b56a8b44315a0fa38a6b27e7b6ec5564
Edgars <indrikis38@gmail.com>
2024-11-25 12:28:15 +0200

    Fixed price 3 for alloy rims

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -919,7 +919,7 @@
         $rim->used = 0;
         $rim->price1 = ceil((round(($item->NetPrice * 1.21), 2) + 15) / 0.7);
         $rim->price2 = $item->Price;
-        $rim->price3 = floor(round($item->RetailPrice * 1.21, 2));
+        $rim->price3 = floor(round($item->RetailPrice * 1.21, 2)) - 2;
         $rim->offer = 0;
         $rim->priceOffer = 0;
         if ($newRim == true) {
